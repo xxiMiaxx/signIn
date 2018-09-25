b00add38769903b1f5bbfe7542a87391a153f812
ArwaHamed <32874102+ArwaHamed@users.noreply.github.com>
2018-09-25 23:03:09 +0300

    Initial Commit

---
+++ signIn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  signIn
+//
+//  Created by Arwa Hamed on 15/01/1440 AH.
+//  Copyright © 1440 Arwa Hamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
